c397f1dbbc9957de49f24772602b6a360f39633b
3DXScape <steve@opensiteplan.org>
2023-02-12 22:27:07 -0800

    Minor updates to C# GeoPose Library copied from CityGML glTF
    Former-commit-id: 7a87ed9e4d3e9eb8ffd1d933a00f022f1ed037d8

--- Implementations/glTF/C#/src/GeoPose.cs
+++ Implementations/glTF/C#/src/GeoPose.cs
@@ -1,4 +1,5 @@
-Copyright (c) 2023 The Dani Elenga Foundation
+/*
+ * Copyright (c) 2023 The Dani Elenga Foundation
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -17,6 +18,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
 
 // Implemention order: 5 - follows Orientation.
 // This is the root of the GeoPose inheritance hierarchy.
--- Implementations/glTF/C#/src/Local.cs
+++ Implementations/glTF/C#/src/Local.cs
@@ -1,4 +1,5 @@
-Copyright (c) 2023 The Dani Elenga Foundation
+/*
+ * Copyright (c) 2023 The Dani Elenga Foundation
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -17,6 +18,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
 
 // Implemention order: 8 -a useful GeoPose for working within a local Cartesian (i.e. engineering) frame.
 // Local can be expressed as an Advanced form, but the Advanced form is more complex and this implementation is a shortcut.
